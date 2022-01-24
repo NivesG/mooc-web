db4420412709d074fab5681362f905b574efbd7d
NivesG <nives.gabrijan@gmail.com>
2022-01-24 14:57:40 +0100

    phonebook 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '05535433'}
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState ('')
+
+  const [searchresults, setSearchResults] = useState(persons)
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -12,10 +16,12 @@ const App = () => {
     if (persons.some(person => person.name === newName)) {
       window.alert(`${newName} is already added to phonebook`);
       setNewName('')
+      setNewNumber('')
     }else{
-      const newPerson = {name: newName}
+      const newPerson = {name: newName, number: newNumber}
       setPersons(persons.concat(newPerson))
       setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -23,28 +29,50 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearch =(event) => {
+    setSearch(event.target.value)
+    const filtered = persons.filter((person) => person.name.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()))
+    setSearchResults(filtered)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addPerson}
+
+      <input 
+        type="text"
+        placeholder="search by name"
+        value={search}
+        onChange={handleSearch}
+        />
+
+      <div>
+        <h2>add new</h2>
+        <form onSubmit={addPerson}
            >
         <div>
           name: <input
           value={newName}
           onChange={handleNameChange} />
         </div>
+        <div>number: <input 
+          value={newNumber}
+          onChange={handleNumberChange}
+        /></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      <div>
-        {persons.map(perso => <p key={perso.name}>{perso.name}</p>)}
-
 
       </div>
       
-      ...
+      <h2>Numbers</h2>
+        {searchresults.map(perso => <p key={perso.name}>{perso.name} {perso.number}</p>)}
+      
     </div>
   )
 }
