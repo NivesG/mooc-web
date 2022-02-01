7a1d85e72ec472a0f5f8389d4356e2d9c021759f
NivesG <nives.gabrijan@gmail.com>
2022-02-01 20:01:41 +0100

    blog test 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,40 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let sum = 0
+  blogs.map((blog) => {
+    sum += blog.likes
+  })
+  return sum
+}
+
+const favouriteBlog = (blogs) => {
+
+  let fav = {
+    title: '',
+    author: '',
+    likes: 0
+  }
+
+  if (!blogs || blogs.length === 0) {
+    return null
+  }
+
+  blogs.map((blog) => {
+    if (blog.likes > fav.likes) {
+      fav = {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+      }
+    }
+  })
+  return fav
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favouriteBlog
 }
