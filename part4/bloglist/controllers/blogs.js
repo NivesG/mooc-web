87f8262554266cef790c6672349e5ac4f80a9590
NivesG <nives.gabrijan@gmail.com>
2022-02-04 20:30:15 +0100

    4.13, 4.14

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,4 +1,6 @@
 const blogRouter = require('express').Router()
+const { response } = require('express')
+const { request } = require('../app')
 const Blog = require('../models/blog')
 
 
@@ -12,5 +14,29 @@ blogRouter.post('/', async (request, response) => {
   const blogs = await blog.save()
   response.status(201).json(blogs)
 })
+
+blogRouter.delete('/:id', async (request, response) => {
+  const id = request.params.id
+  await Blog.findByIdAndRemove(id)
+  response.status(204).end()
+})
+
+blogRouter.put('/:id', async (request, response) => {
+  const { body } = request
+  const { id } = request.params
+
+  const blog = {
+    likes: body.likes,
+  }
+
+  const updatedBlog = await Blog.findByIdAndUpdate(id, blog, { new: true })
+
+  if (updatedBlog) {
+    response.status(200).json(updatedBlog.toJSON())
+  } else {
+    response.status(404).end()
+  }
+})
+
 
 module.exports = blogRouter
