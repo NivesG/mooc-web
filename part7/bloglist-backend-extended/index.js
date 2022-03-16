ab38a52e991c806cf4969e48816180dc349745ed
NivesG <nives.gabrijan@gmail.com>
2022-03-16 19:00:33 +0100

    heroku

--- part7/bloglist-backend-extended/index.js
+++ part7/bloglist-backend-extended/index.js
@@ -1,10 +1,9 @@
-
 const app = require('./app')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
+const PORT = process.env.PORT || 3001
 
-
-app.listen(config.PORT, () => {
+app.listen(PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
