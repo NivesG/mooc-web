8669bed35b76caa462f8f52fbe83b155a1f34d46
NivesG <nives.gabrijan@gmail.com>
2022-06-07 13:28:00 +0200

    9.8

---
+++ part9/patientor/build/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const PORT = 3001;
+app.get('/api/ping', (_req, res) => {
+    console.log('someone pinged here');
+    res.send('pong');
+});
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
